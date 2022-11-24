74405df5c406b3e5ffede2c2715e891650fb1ec0
sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-24 17:54:40 +0500

    create function

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request 
+from rest_framework import status
 
-# Create your views here.
+# serializer
+from .serializers import UserSerializer, TaskSerializer
+
+# CRUD
+@api_view(['POST'])
+def create_task(request: Request) -> Response:
+    # getting data 
+    data = request.data
+    serializer = TaskSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_204_NO_CONTENT)
+    
+
+
