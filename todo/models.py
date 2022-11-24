46e003bf879c756bbe48f70ba527106cb80d501a
sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-24 17:30:52 +0500

    created user and task

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=12)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updateAt = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
